e9375cb309afa02240e65d54cac2ca6635b9fd69
daraxdray <daraxdray86@gmail.com>
2019-11-02 14:33:48 +0100

    flutter and paystack ready
    All paystack and flutterwave login ready

---
+++ js/paystackapp.js
@@ -0,0 +1,25 @@
+function payWithPaystack(){
+    var handler = PaystackPop.setup({
+      key: 'pk_test_50c50bba0d09666cc46820850f20853b3628f06d',
+      email: 'customer@email.com',
+      amount: 10000,
+      currency: "NGN",
+      ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+      metadata: {
+         custom_fields: [
+            {
+                display_name: "Mobile Number",
+                variable_name: "mobile_number",
+                value: "+2348012345678"
+            }
+         ]
+      },
+      callback: function(response){
+          alert('success. transaction ref is ' + response.reference);
+      },
+      onClose: function(){
+          alert('window closed');
+      }
+    });
+    handler.openIframe();
+  }
--- js/vueapp.js
+++ js/vueapp.js
@@ -1,3 +1,4 @@
+
 var currency = [
     '(Af)',								
 																				
@@ -304,6 +305,9 @@ new Vue({
         one:true,
         timeRate:null,
         show:false,
+        isPayFlutter:'',
+        isPayPaystack:'',
+        API_publicKey:"FLWPUBK_TEST-c4f6ace72bd66767285d1da656847f8e-X",
         usr:{
             phone:'',
             reference:'',
@@ -313,9 +317,11 @@ new Vue({
             email:'',
             times:'',
             country:'Nigeria',
-            currency:'Naira'
+            currency:'',
+            amount:''
         },
-        currency: currency
+        currency: currency,
+        allCountries:''
 
     },
 
@@ -325,11 +331,89 @@ new Vue({
         }
     },
 methods:{
+    getCountries(){
+    axios.get('https://restcountries.eu/rest/v2/all')
+    .then(res=>{
+        console.log(res.data)
+        this.allCountries = res.data
+    },
+    err=>console.log(err))        
+        
+
+    },
+    selectedPay(par){
+        if(par == 1){
+        this.isPayFlutter = true
+        this.isPayPaystack = false
+        }
+        else{
+            this.isPayPaystack = true
+            this.isPayFlutter = false
+        }
+    }
+    ,
+    paystackapp(){
+        var inst = this; 
+        console.log(this.usr.amount)
+        var handler = PaystackPop.setup({
+            key: 'pk_test_50c50bba0d09666cc46820850f20853b3628f06d',
+            email: this.usr.email,
+            amount: 4000000,
+            currency: this.usr.currency,
+            metadata: {
+               custom_fields: [
+                  {
+                      display_name: "Mobile Number",
+                      variable_name: "mobile_number",
+                      value: this.usr.phone
+                  }
+               ]
+            },
+            callback: function(response){
+                alert('success. transaction ref is ' + response.reference);
+            },
+            onClose: function(){
+                inst.$refs.paymentRes.click();
+            }
+          });
+          handler.openIframe();
+    },
+    raveapp(){
+        console.log('rave is active')
+       
+            var x = getpaidSetup({
+                PBFPubKey: this.API_publicKey,
+                customer_email: this.usr.email,
+                amount: this.usr.amount,
+                customer_phone: this.usr.phone,
+                currency: this.usr.currency,
+                txref: "rave-123456",
+                meta: [{
+                    metaname: "user_name",
+                    metavalue: this.fullname
+                }],
+                onclose: function() {},
+                callback: function(response) {
+                    var txref = response.tx.txRef; // collect txRef returned and pass to a 					server page to complete status check.
+                    console.log("This is the response returned after a charge", response);
+                    if (
+                        response.tx.chargeResponseCode == "00" ||
+                        response.tx.chargeResponseCode == "0"
+                    ) {
+                    this.goto()
+                    } else {
+                        // redirect to a failure page.
+                    }
+    
+                    x.close(); // use this to close the modal immediately after payment.
+                }
+            });
+        
+    },
     goto(){
-        window.location = "registration_send.html"
+       return window.location = "registration_send.html"
     },
     formatCurrency(value){
-        console.log(value)
       return this.currency[value-1]
 
     }
@@ -340,6 +424,10 @@ methods:{
                 this.show = true
             }
         }
+    },
+
+    mounted(){
+        this.getCountries()
     }
 
 
